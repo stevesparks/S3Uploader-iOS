2e0a5206da292bdb9117d92420603036f61e0a1a
Steve Sparks <sparks@bignerdranch.com>
2016-11-20 09:15:50 -0500

    Initial

---
+++ S3Uploader/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  S3Uploader
+//
+//  Created by Steve Sparks on 11/20/16.
+//  Copyright © 2016 Big Nerd Ranch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
